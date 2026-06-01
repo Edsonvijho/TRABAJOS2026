2a7d48c7e02a85e3a2ba2909730c3da064cf9da2
Edsonvijho <sorianoedson87@gmail.com>
2026-06-01 10:08:49 -0500

    Add files via upload

--- TablaMultiplicar.java
+++ TablaMultiplicar.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class TablaMultiplicar {
     public static void main(String[] args) {
-        int numero = 5;
-        System.out.println("Tabla del " + numero);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int num = sc.nextInt();
+
         for (int i = 1; i <= 10; i++) {
-            System.out.println(numero + " x " + i + " = " + (numero * i));
+            System.out.println(num + " x " + i + " = " + (num * i));
         }
+
+        sc.close();
     }
 }
